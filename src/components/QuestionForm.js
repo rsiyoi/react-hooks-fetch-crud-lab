f87e0031e2c4521ee562ac0732777f0fc8225f94
Ruth Siyoi <ruth.siyoi@gmail.com>
2025-06-03 18:09:26 +0300

    CRUD lab

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -20,7 +20,43 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
-  }
+    const newQuestion = {
+    prompt: formData.prompt,
+    answers: [
+      formData.answer1,
+      formData.answer2,
+      formData.answer3,
+      formData.answer4,
+    ],
+    correctIndex: parseInt(formData.correctIndex),
+  };
+
+  fetch("http://localhost:4000/questions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newQuestion),
+  })
+    .then((res) => res.json())
+    .then((createdQuestion) => {
+      // Call parent callback if it exists
+      if (props.onAddQuestion) {
+        props.onAddQuestion(createdQuestion);
+      }
+      // Reset form
+      setFormData({
+        prompt: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "",
+        correctIndex: 0,
+      });
+    })
+    .catch((error) => console.error("Error adding question:", error));
+}
+
 
   return (
     <section>

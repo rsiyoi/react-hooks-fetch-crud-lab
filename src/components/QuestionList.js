c21be05fc5d1842b863659c6fd499ad4d4661b34
Ruth Siyoi <ruth.siyoi@gmail.com>
2025-06-03 11:30:21 +0300

    adds delete and correct answer functonality

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,58 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({ questions, onDeleteQuestion, onUpdateQuestion}) {
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE'
+    }).then(() => onDeleteQuestion(id));
+  }
+
+  function handleChangeCorrectAnswer(id, newIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ correctIndex: parseInt(newIndex) })
+    })
+      .then(res => res.json())
+      .then(updatedQuestion => onUpdateQuestion(updatedQuestion));
+  }
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+        {questions.map(question => (
+          <li key={question.id}>
+            <strong>{question.prompt}</strong>
+            <ul>
+              {question.answers.map((answer, idx) => (
+                <li key={idx}>
+                  {answer} {idx === question.correctIndex && '(✔️ correct)'}
+                </li>
+              ))}
+            </ul>
+            <label>
+              Change Correct Answer:
+              <select
+                value={question.correctIndex}
+                onChange={e =>
+                  handleChangeCorrectAnswer(question.id, e.target.value)
+                }
+              >
+                {question.answers.map((_, idx) => (
+                  <option key={idx} value={idx}>
+                    {idx}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <button onClick={() => handleDelete(question.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
